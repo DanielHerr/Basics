2e4daf73bb40bf25b69139155db8c007f5c8a72c
Daniel Herr <DanielHerr@users.noreply.github.com>
2016-04-13 20:58:37 -0400

    Update basics.js

--- basics.js
+++ basics.js
@@ -7,6 +7,18 @@ function wait(time = 0) {
   setTimeout(resolve, time)
 })) }
 
+function datatype(data) {
+ let type = typeof(data)
+ if(type == "object") {
+  if(data == null) {
+   type = "null"
+  }
+  else if(Array.isArray(data)) {
+   type = "array"
+ } }
+ return(type)
+}
+
 var log = console.log.bind(console)
 
 EventTarget.prototype.on = EventTarget.prototype.addEventListener
